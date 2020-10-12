21905984cbb8f9ca7144dd13fff9f579425f1d4f
Irfan Maulana <irfanmaulana007@icloud.com>
2020-10-12 11:30:36 +0700

    Updates

---
+++ precache-manifest.d7c740d4a9226f3a9565914e4361be45.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bffdfc9b508bad8d0f5da1a3670eaa8",
+    "url": "/semut-merah-informatika/index.html"
+  },
+  {
+    "revision": "517842858f5ea399e3bd",
+    "url": "/semut-merah-informatika/static/css/2.d729c219.chunk.css"
+  },
+  {
+    "revision": "037b8147a0e90bf38dd7",
+    "url": "/semut-merah-informatika/static/css/main.6ca9c12e.chunk.css"
+  },
+  {
+    "revision": "517842858f5ea399e3bd",
+    "url": "/semut-merah-informatika/static/js/2.02c49981.chunk.js"
+  },
+  {
+    "revision": "42dc647c1d2ebc2e0a5e88272d442d98",
+    "url": "/semut-merah-informatika/static/js/2.02c49981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037b8147a0e90bf38dd7",
+    "url": "/semut-merah-informatika/static/js/main.0e7f778e.chunk.js"
+  },
+  {
+    "revision": "75a78e883b378d7f962c",
+    "url": "/semut-merah-informatika/static/js/runtime-main.d3c04cca.js"
+  },
+  {
+    "revision": "fff8f0fc7cf1c2d7766e5f4856ea61c5",
+    "url": "/semut-merah-informatika/static/media/amman_mineral_logo.fff8f0fc.png"
+  },
+  {
+    "revision": "b4152264d59827b269015e2a4535069b",
+    "url": "/semut-merah-informatika/static/media/bg1.b4152264.jpg"
+  },
+  {
+    "revision": "514cba5bf016437d0ad2b0c41712cf25",
+    "url": "/semut-merah-informatika/static/media/bg2.514cba5b.jpg"
+  },
+  {
+    "revision": "d940d9044628f19d0f808ad6af23192a",
+    "url": "/semut-merah-informatika/static/media/bpjs_logo.d940d904.png"
+  },
+  {
+    "revision": "9f101b42d3aea8e70e589f7621ec3981",
+    "url": "/semut-merah-informatika/static/media/bpk_logo.9f101b42.png"
+  },
+  {
+    "revision": "d7faf9fc94c3d6ed924597902a4d77c3",
+    "url": "/semut-merah-informatika/static/media/braincode_logo.d7faf9fc.png"
+  },
+  {
+    "revision": "964a33b8e74c48496e73865ba0a0ca25",
+    "url": "/semut-merah-informatika/static/media/bri_logo.964a33b8.png"
+  },
+  {
+    "revision": "16c9314ddef9b478ceae2308ec036872",
+    "url": "/semut-merah-informatika/static/media/btpn_logo.16c9314d.png"
+  },
+  {
+    "revision": "181c33ed3fb2243165580de731d1d098",
+    "url": "/semut-merah-informatika/static/media/indokordsa_logo.181c33ed.png"
+  },
+  {
+    "revision": "1cff49c0d785a6b01b0d329bd0df56f2",
+    "url": "/semut-merah-informatika/static/media/kemdiknas_logo.1cff49c0.png"
+  },
+  {
+    "revision": "7044e758714b6ee85c65157fc781df94",
+    "url": "/semut-merah-informatika/static/media/mandiri_logo.7044e758.png"
+  },
+  {
+    "revision": "ef761e415224e325a39af75be6810eec",
+    "url": "/semut-merah-informatika/static/media/pertamina_logo.ef761e41.png"
+  },
+  {
+    "revision": "53d348d3374dbc1a74a46c1c24a3c1db",
+    "url": "/semut-merah-informatika/static/media/rs_indramayu_logo.53d348d3.png"
+  },
+  {
+    "revision": "9613c6e03016e21c230a34c3c479cbbd",
+    "url": "/semut-merah-informatika/static/media/siloam_logo.9613c6e0.png"
+  },
+  {
+    "revision": "65a2728ef2fd7e95fb52c488785c4272",
+    "url": "/semut-merah-informatika/static/media/smi_logo_small.65a2728e.png"
+  },
+  {
+    "revision": "8e8fddd21ed8f0f829ded3c97eac3231",
+    "url": "/semut-merah-informatika/static/media/toyota_logo.8e8fddd2.png"
+  },
+  {
+    "revision": "66cdc2fb595f679a789e07365be208ab",
+    "url": "/semut-merah-informatika/static/media/tugu_logo.66cdc2fb.png"
+  },
+  {
+    "revision": "df841f703eb9ce87f2d687a599966b1f",
+    "url": "/semut-merah-informatika/static/media/utpe_logo.df841f70.png"
+  }
+]);
